d5ebf31e7801349296030dc0741a22df8e35b730
Raggz291 <nicole.muraguri@strathmore.edu>
2024-07-10 17:56:13 +0300

    update

---
+++ banner.php
@@ -0,0 +1,7 @@
+<section class="bakery-banner">
+    <div class="banner-content">
+        <h1>Welcome to Ragzzy Bakery</h1>
+        <p>Delight in our warm, freshly baked goods and exquisite flavors crafted with care</p>
+        <a href="#" class="cta-button">Order Now</a>
+    </div>
+</section>
---
+++ special-offers.php
@@ -0,0 +1,18 @@
+<section class="special-offers">
+    <h2>Special Offers</h2>
+    <div class="offer">
+        <img src="img/cakeToo.jpg" alt="Special Offer 1"> 
+        <img src="img/cakey.jpg" alt="Special Offer 2">
+        <img src="img/cakes.jpg" alt="Special Offer 3">
+        <h3>10% Off on our Bento cakes for every 2 purchases</h3>
+        <p>Enjoy a mouthwatering selection of cakes at a discounted price.</p>
+        
+    </div>
+    <div class="offer">
+        <img src="img/croissant.jpeg" alt="Special Offer 4">
+        <img src="img/baguette.jpeg" alt="Special Offer 5">
+        <img src="img/pastries.jpg" alt="Special Offer 6">
+        <h3>Buy One, Get One Free</h3>
+        <p>Get a free pastry with every purchase of a specialty bread.</p>
+    </div>
+</section>
